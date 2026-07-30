a98e11bfdbbefd412d20b3820207518e99314ffe
sairam <sairamarige2140@gmail.com>
2026-07-30 11:20:33 +0530

    Fix typo in DATABASE_URL connection string

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
 
 # DATABASE_URL="mysql+pymysql://root:1234@localhost:3306/store_db"
-DATABASE_URL="mysql://avnadmin:AVNS_ltzGQHRHtNxWPlp03iq@mysql-1d69e83f-sairamarige2140-f01e.c.aivencloud.com:16747/defaultdb"
+DATABASE_URL="mysql+pymsql://avnadmin:AVNS_ltzGQHRHtNxWPlp03iq@mysql-1d69e83f-sairamarige2140-f01e.c.aivencloud.com:16747/defaultdb"
 
 engine=create_engine(DATABASE_URL)
 
