1619d1c2f42384722218651f687e1519a59497dd
sairam <sairamarige2140@gmail.com>
2026-08-06 12:38:59 +0530

    Update DATABASE_URL to use environment variable

--- database.py
+++ database.py
@@ -7,8 +7,8 @@ from sqlalchemy.orm import sessionmaker,declarative_base
 
 load_dotenv()
 
-# DATABASE_URL=os.getenv("DB_URL")
-DATABASE_URL=mysql+pymysql://avnadmin:AVNS_ltzGQHRHtNxWPlp03iq@mysql-1d69e83f-sairamarige2140-f01e.c.aivencloud.com:16747/defaultdb
+DATABASE_URL=os.getenv("DB_URL")
+
 
 
 engine=create_engine(DATABASE_URL)
