800e3e9d95eb85a1cd7611402556216d3f1c8c8c
sairam <sairamarige2140@gmail.com>
2026-08-06 12:27:08 +0530

    Change DATABASE_URL to a specific MySQL connection
    Updated DATABASE_URL to use a specific MySQL connection string.

--- database.py
+++ database.py
@@ -7,7 +7,10 @@ from sqlalchemy.orm import sessionmaker,declarative_base
 
 load_dotenv()
 
-DATABASE_URL=os.getenv("DB_URL")
+# DATABASE_URL=os.getenv("DB_URL")
+DATABASE_URL=mysql+pymysql://avnadmin:AVNS_ltzGQHRHtNxWPlp03iq@mysql-1d69e83f-sairamarige2140-f01e.c.aivencloud.com:16747/defaultdb
+
+
 engine=create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
